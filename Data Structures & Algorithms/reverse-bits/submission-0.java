ecad426f4b111c0c90d69dafe953178b2962a9db
tazam777 <62661902+tazam777@users.noreply.github.com>
2026-05-14 17:00:54 -0600

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result=0;
+        for(int i=0;i<32;i++)
+        {
+            int lastbit= n&1;
+            result =result<< 1;
+
+            result=result|lastbit;
+
+            n=n>>>1;
+
+        }
+        return result;
+        
+    }
+}
