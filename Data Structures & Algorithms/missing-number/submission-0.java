b0cf409c37b77855881f4b6e84530205159af4a9
tazam777 <62661902+tazam777@users.noreply.github.com>
2026-05-14 17:05:02 -0600

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n=nums.length;
+
+        int expectedsum=n*(n+1)/2;
+        int actualsum=0;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            actualsum=actualsum+nums[i];
+
+        }
+
+        return expectedsum-actualsum;
+        
+    }
+}
